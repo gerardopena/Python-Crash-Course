4935f1f4fa0f7cdf85dac442024376ec30830b0b
gerardopena <35822482+gerardopena@users.noreply.github.com>
2020-11-21 20:24:28 -0800

    Update

---
+++ Part 1 Basics/Chapter 2 Variables and Simple Data Types/numbers.py
@@ -0,0 +1,54 @@
+# Integers
+print(2 + 3)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2)
+
+# Two multiplication symbols to represent exponents
+print(3 ** 2)
+print(3 ** 3)
+print(10 ** 6)
+
+# Order of operations
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+
+# Floats
+print(0.1 + 0.1)
+print(0.2 + 0.2)
+print(2 * 0.1)
+print(2 * 0.2)
+
+# Sometimes get an arbitrary number of demical places in answers
+print(0.2 + 0.1)
+print(3 * 0.1)
+
+# Integers and Floats
+print(4 / 2)
+print(1 + 2.0)
+print(2 * 3.0)
+print(3.0 ** 2)
+
+# Underscores in Numbers
+universe_age = 14_000_000_000
+print(universe_age)
+
+# Multiple Assingment
+x, y, z = 0, 1, 2
+print(x)
+print(y)
+print(z)
+
+# Contstants
+MAX_CONNECTIONS = 5000
+print(MAX_CONNECTIONS)
+
+# 2.8 Number Eight
+print(5+3)
+print(12-4)
+print(4*2)
+print(24/3)
+
+# 2.9 Favorite Number
+favorite_num = 777
+print(f'My favorite number is {favorite_num}')
